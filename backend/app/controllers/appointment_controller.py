a957db80d004fc1e97f618063279ee1dae655aba
DeepthiMoolathingal <20034684@mydbs.ie>
2024-07-14 11:57:58 +0100

    updates

--- backend/app/controllers/appointment_controller.py
+++ backend/app/controllers/appointment_controller.py
@@ -1,16 +1,19 @@
 import logging
 import traceback
 from flask import Blueprint, request, jsonify
+from flask_cors import cross_origin
 from app.models import Appointment
 from app import db
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
+import datetime
 appointment_bp = Blueprint('appointment', __name__)
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 @appointment_bp.route('/appointments', methods=['POST'])
+@cross_origin(supports_credentials=True)
 @jwt_required()
 def create_appointment():
     try:
@@ -25,7 +28,7 @@ def create_appointment():
 
         new_appointment = Appointment(
             service_type=service_type,
-            appointment_date=appointment_date,
+            appointment_date=datetime.datetime.strptime(appointment_date,'%Y-%m-%dT%H:%M'),
             user_id=current_user['id']
         )
         db.session.add(new_appointment)
@@ -38,12 +41,14 @@ def create_appointment():
         return jsonify({'message': 'Internal Server Error'}), 500
 
 @appointment_bp.route('/appointments', methods=['GET'])
+@cross_origin(supports_credentials==True)
 @jwt_required()
 def get_appointments():
     appointments = Appointment.query.all()
     return jsonify([appointment.to_dict() for appointment in appointments]), 200
 
 @appointment_bp.route('/appointments/<int:id>', methods=['GET'])
+@cross_origin(supports_credentials==True)
 @jwt_required()
 def get_appointment(id):
     appointment = Appointment.query.get_or_404(id)
@@ -51,6 +56,7 @@ def get_appointment(id):
 
 @appointment_bp.route('/appointments', methods=['POST'])
 @jwt_required()
+@cross_origin(supports_credentials==True)
 def book_appointment():
     current_user = get_jwt_identity()
     data = request.get_json()
@@ -66,6 +72,7 @@ def book_appointment():
 
 @appointment_bp.route('/appointments/<int:id>', methods=['PUT'])
 @jwt_required()
+@cross_origin(supports_credentials==True)
 def update_appointment(id):
     appointment = Appointment.query.get_or_404(id)
     data = request.get_json()
@@ -79,6 +86,7 @@ def update_appointment(id):
 
 @appointment_bp.route('/appointments/<int:id>', methods=['DELETE'])
 @jwt_required()
+@cross_origin(supports_credentials==True)
 def delete_appointment(id):
     appointment = Appointment.query.get_or_404(id)
     db.session.delete(appointment)
